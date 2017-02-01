430a13b0eda1b7b1f1e370291620b5dca59c1037
mbasmanov92 <mbasmanon92@gmail.com>
2017-02-01 13:52:52 +0700

    не снимается первый класс pin--active

--- js/form.js
+++ js/form.js
@@ -1,6 +1,37 @@
 var tokyoPinMap = document.querySelector('.tokyo__pin-map');
-var pin = tokyoPinMap.querySelector('.pin');
-var clickPin = function() {
-  pin.classList.add('pin--active');
-};
-pin.addEventListener('click', clickPin);
+var dialog = document.querySelector('.dialog');
+var dialogClose = document.querySelector('.dialog__close');
+var clickPin;
+var target;
+var dialogClickOpen;
+var dialogClickClose;
+
+dialogClickOpen = function(event) {
+  target = event.target;
+  while (target != this) {
+    if (target.className === 'pin') {
+      highlight(target);
+      return;
+    }
+    target = target.parentNode;
+  }
+}
+
+function highlight(node) {
+  if (clickPin) {
+    clickPin.classList.remove('pin--active');
+  }
+  clickPin = node;
+  clickPin.classList.add('pin--active');
+  dialog.style.display = "block";
+}
+
+tokyoPinMap.addEventListener ('click', dialogClickOpen);
+
+dialogClickClose = function() {
+  dialog.style.display = "none";
+  var clickActive = tokyoPinMap.querySelector('.pin--active');
+  clickActive.classList.remove('pin--active');
+}
+
+dialogClose.addEventListener ('click', dialogClickClose);
