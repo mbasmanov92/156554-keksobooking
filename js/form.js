8e4a1e29b5b4b72148799064ad3f8132afdd7c33
mbasmanov92 <mbasmanon92@gmail.com>
2017-02-04 16:33:43 +0700

    валидация форм сделана

--- js/form.js
+++ js/form.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var pins = document.querySelectorAll('.pin');
 var activePin = document.querySelector('.pin.pin--active');
 var dialog = document.querySelector('.dialog');
@@ -8,22 +7,33 @@ var dialogClickClose;
 var title;
 var price;
 var address;
+var time;
+var timeout;
+var changeTimeout;
+var changeTime;
+var type;
+var changeType;
+var typeValue;
+var roomNumber;
+var capacity;
+var changeRoomNumber;
+var roomNumberValue;
 Array.prototype.forEach.call(pins, function (pin) {
   pin.addEventListener('click', function () {
     if (activePin) {
       activePin.classList.remove('pin--active');
     }
     pin.classList.add('pin--active');
-    dialog.style.display = "block";
+    dialog.style.display = 'block';
     activePin = pin;
   });
 });
 
 dialogClickClose = function () {
-  dialog.style.display = "none";
+  dialog.style.display = 'none';
   activePin.classList.remove('pin--active');
-}
-dialogClose.addEventListener ('click', dialogClickClose);
+};
+dialogClose.addEventListener('click', dialogClickClose);
 
 title = document.querySelector('#title');
 title.setAttribute('required', true);
@@ -38,30 +48,52 @@ price.setAttribute('max', 1000000);
 address = document.querySelector('#address');
 address.setAttribute('required', true);
 
-var time = document.querySelector('#time');
-var timeout = document.querySelector('#timeout');
-var optionsTime = time.getElementsByTagName('option');
-var optionsTimeout = timeout.getElementsByTagName('option');
-var value;
-var selectedTime = function() {
-  Array.prototype.forEach.call(optionsTime, function (option) {
-    if (option.hasAttribute('selected')) {
-      value = option.getAttribute('value');
- console.log(value);
-      return;
-    };
-  });
-  Array.prototype.forEach.call(optionsTimeout, function (option) {
-    if (option.hasAttribute('selected')) {
-      option.removeAttribute('value');
-      return;
-    } else {
-    if (option.getAttribute('value') === value) {
-      option.setAttribute('selected');
-      return;
-    };
-    };
-  });
+time = document.querySelector('#time');
+timeout = document.querySelector('#timeout');
+changeTimeout = function () {
+  timeout.selectedIndex = time.selectedIndex;
 };
-time.addEventListener('click', selectedTime);
+changeTime = function () {
+  time.selectedIndex = timeout.selectedIndex;
+};
+time.addEventListener('change', changeTimeout);
+timeout.addEventListener('change', changeTime);
 
+type = document.querySelector('#type');
+changeType = function () {
+  typeValue = type.value;
+  switch (typeValue) {
+    case 'Квартира':
+      price.setAttribute('min', 5000);
+      price.setAttribute('placeholder', 5000);
+      break;
+    case 'Лачуга':
+      price.setAttribute('min', 0);
+      price.setAttribute('placeholder', 0);
+      break;
+    case 'Дворец':
+      price.setAttribute('min', 10000);
+      price.setAttribute('placeholder', 10000);
+      break;
+  }
+};
+type.addEventListener('change', changeType);
+
+roomNumber = document.querySelector('#room_number');
+capacity = document.querySelector('#capacity');
+
+changeRoomNumber = function () {
+  roomNumberValue = roomNumber.value;
+  switch (roomNumberValue) {
+    case '1 комната':
+      capacity.options[1].selected = true;
+      break;
+    case '2 комнаты':
+      capacity.options[0].selected = true;
+      break;
+    case '100 комнат':
+      capacity.options[0].selected = true;
+      break;
+  }
+};
+roomNumber.addEventListener('change', changeRoomNumber);
